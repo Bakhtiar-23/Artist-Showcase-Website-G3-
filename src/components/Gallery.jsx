333a898462585e4a7c07ad8978f0539572856c82
Bakhtiar Ismail <124157979+Bakhtiar-23@users.noreply.github.com>
2024-12-04 18:31:24 +0200

    Updating some errors

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,7 +5,7 @@ import '../App.css'; // Import common styles
 const galleryItems = [
   { id: 1, src: '/assets/watercolor1.avif', size: '24x36 cm', price: '€50' },
   { id: 2, src: '/assets/watercolor2.avif', size: '30x40 cm', price: '€70' },
-  { id: 3, src: '/assets/watercolor3.avif', size: '50x45 cm', price: '€100' },
+  { id: 3, src: '/assets/watercolor3.avif', size: '50x45 cm', price: '€1000' },
 ];
 
 const Gallery = () => {
